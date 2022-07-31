31bc4694b35be9ac9810f0dc8271e86972d98903
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-07-31 16:03:23 +0500

    Style the app

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
         return (
             <div className='tc' >
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChanges} />
                 <CardList robots={filteredRobots} />
             </div>
