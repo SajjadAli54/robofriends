a27d97e8e1501850dd2a1a17fd1c3f2f6708da82
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-07-31 15:06:17 +0500

    Refactore code Include App Components

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { StrictMode } from 'react';
-import { robots } from './robots'
+import App from './App'
 import 'tachyons'; // npm install tachyons
-import CardList from './MyComponents/CardList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <CardList robots={robots} />
+    <App />
   </StrictMode>
 );
 
