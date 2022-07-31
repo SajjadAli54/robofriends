8a083de19467443c9ff5b847e37438d8d706f11d
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-07-31 13:00:31 +0500

    Added a new component Hello with its basic css Checked it in index.js Introduced tachyons but couldn't use due to net issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StrictMode } from 'react';
+import Hello from './MyComponents/Hello';
+// import 'tachyons'; // npm install tachyons
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <Hello greeting={'Hello' + ' React Ninja'} />
   </StrictMode>
 );
 
