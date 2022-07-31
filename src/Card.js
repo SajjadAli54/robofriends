4ab7c2a063c270139ab05b8eb112231816382038
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-07-31 18:43:10 +0500

    Removed an error left previously

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Card({ id, name, email }) {
-    let i = Math.round(Math.random) + picsArray.length - 1;
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw shadow-5'>
             <img src={`https://robohash.org/${id}?200x200`} alt='Robot Image' />
