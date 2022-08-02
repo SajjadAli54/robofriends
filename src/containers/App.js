a94aaa3304cab399888abf9910a17c9d9d29bcd3
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-08-02 10:14:36 +0500

    Remove a comment

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,10 +31,9 @@ class App extends Component {
     }
 
     componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response => response.json())
-        //     .then(users => this.setState({ robots: users }))
-        this.setState({ robots: users })
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     render() {
